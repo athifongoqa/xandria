53784aee664b4f53037c0ebb9926dde9c6df7412
Athi Fongoqa <fongoqaathi@gmail.com>
2023-02-27 00:54:29 +0100

    add sidebar + prompt examples

---
+++ components/SideBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import NewChat from './NewChat'
+
+function SideBar() {
+  return (
+    <div className='p-2 flex flex-col h-screen'>
+        <div className='flex-1'>
+            <div>
+                {/* New Chat */}
+                <NewChat />
+
+                <div>
+                    {/* Model Selection */}
+                </div>
+
+                {/* Map through Chat rows */}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
