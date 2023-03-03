848fa157446b6bf92bd3091c4986ebb81ce906c6
Athi Fongoqa <fongoqaathi@gmail.com>
2023-03-03 18:52:53 +0100

    feat: add nextauth w/ Google provider

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -5,7 +5,7 @@ function SideBar() {
   return (
     <div className='p-2 flex flex-col h-screen'>
         <div className='flex-1'>
-            <div>
+            <div> 
                 {/* New Chat */}
                 <NewChat />
 
