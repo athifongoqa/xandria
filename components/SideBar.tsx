c58690943741d9624181c8861d9e4feb2c02dd77
Athi Fongoqa <fongoqaathi@gmail.com>
2023-03-08 14:45:50 +0100

    feat: add signout

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+'use client'
+
+import { useSession, signOut } from 'next-auth/react'
 import NewChat from './NewChat'
 
 function SideBar() {
+  const { data: session } = useSession();
   return (
     <div className='p-2 flex flex-col h-screen'>
         <div className='flex-1'>
@@ -16,8 +19,13 @@ function SideBar() {
                 {/* Map through Chat rows */}
             </div>
         </div>
+
+        {session && <img src={session.user?.image!} alt='' 
+        className='h-12 w-12 rounded-full cursor-pointer hover:opacity-50 mx-auto mb-2'
+        onClick={() => signOut()}
+        />}
     </div>
-  )
+  );
 }
 
 export default SideBar
